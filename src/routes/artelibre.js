cebcf3c00e918b11a510693f7aa10342f0954310
Luis Fernando Mendez <luisfernando.mendez5@gmail.com>
2020-04-07 16:36:35 -0400

    .

--- src/routes/artelibre.js
+++ src/routes/artelibre.js
@@ -4,79 +4,14 @@ const router = express.Router();
 
 //-------------------VISTAS DEL ADMINISTRADOR---------------------//
 
-router.get('/admin-artistas', (req, res) => { 
-    res.render('admin/artistas');
-});
 
-router.get('/admin-colecciones', (req, res) => { 
-    res.render('admin/colecciones');
-});
-  
-router.get('/admin-estadisticas', (req, res) => { 
-    res.render('admin/estadisticas');
-});
-
-router.get('/admin-eventos', (req, res) => { 
-    res.render('admin/eventos');
-});
-
-router.get('/admin-obras', (req, res) => { 
-    res.render('admin/obras');
-});
-
-router.get('/admin-subastas', (req, res) => { 
-    res.render('admin/subastas');
-});
-
-router.get('/admin-estadisticas', (req, res) => { 
-    res.render('admin/tasas');
-});
 
 
   //-------------------VISTAS DEL ARTISTA---------------------//
-router.get('/nueva-obra', (req, res) => {
-    res.render('artist/nueva-obra')
-})
 
-  router.get('/artistas-coleccion', (req, res) => { 
-    res.render('artistas/coleccion');
-});
-
-router.get('/artistas-estadisticas', (req, res) => { 
-    res.render('artistas/estadisticas');
-});
-
-router.get('/artistas-eventos', (req, res) => { 
-    res.render('artistas/evento');
-});
-
-router.get('/artistas-home', (req, res) => { 
-    res.render('artistas/home');
-});
-
-router.get('/artistas-mis-colecciones', (req, res) => { 
-    res.render('artistas/mis-colecciones');
-});
-
-router.get('/artistas-mis-obras', (req, res) => { 
-    res.render('artistas/mis-obras');
-});
-
-router.get('/artistas-perfil', (req, res) => { 
-    res.render('artistas/perfil');
-});
-
-router.get('/artistas-mis-ventas', (req, res) => { 
-    res.render('artistas/mis-ventas');
-});
-
-router.get('/artistas-mis-eventos', (req, res) => { 
-    res.render('artistas/mis-eventos');
-});
-
-router.get('/artistas-contacto', (req, res) => { 
-    res.render('artistas/contacto');
-});
+  router.get('/nueva-obra', (req, res) => {
+      res.render('artist/nueva-obra');
+  });
 
 
 
