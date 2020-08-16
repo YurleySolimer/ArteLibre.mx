8da048c016fa6df081fefe34db03c2dcb0623a96
Yurley Solimer <yurley.solimer@gmail.com>
2020-08-16 14:27:36 -0400

    userid

--- src/routes/artelibre.js
+++ src/routes/artelibre.js
@@ -187,8 +187,8 @@ router.get('/obra/:id', async (req, res) => {
   userStripe = await pool.query('SELECT * FROM artistStripe WHERE id_user =? LIMIT 1', [obra[0].artista_id]);
   var uStripe = false;
   if (userStripe.length > 0) {
-    userID = userStripe[0].id_stripe;
-    uStripe = true;
+    var userID = userStripe[0].id_stripe;
+    var uStripe = true;
   }
   const fee = Math.round(obra[0].precio * 0.15);
   const pago = obra[0].precio*100;
